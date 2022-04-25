16967eb466394b5d8d8b785a457fc4620f05df00
nataly <nvchulichkova@yandex.ru>
2022-04-25 23:21:48 +0300

    feat: add API

--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 * Формы
 * Java Script
 * Webpack
+* API
 
 
 **Figma**
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,15 +1,15 @@
 import { pushEsc, handlePopupClose } from './modal.js';
 
 function openPopup(popup) {
-	popup.classList.add('popup_opened');
   document.addEventListener('keydown', pushEsc);
   popup.addEventListener('click', handlePopupClose);
+  popup.classList.add('popup_opened');
 };
 
 function closePopup(popup) {
   document.removeEventListener('keydown', pushEsc);
   popup.removeEventListener('click', handlePopupClose);
-	popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 };
 
 export { openPopup, closePopup };
